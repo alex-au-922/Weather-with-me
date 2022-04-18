a3576c95b5f65739cf3107cfac9755ab9e7787f0
alexau922 <alexuau922@gmail.com>
2022-04-19 01:48:22 +0800

    Added the wrapper

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,22 +1,24 @@
 import React, { useEffect, useContext } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 // create a component so that it has the function of navbar
 export default function NavBar(props) {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="/home">Weather With Me</Navbar.Brand>
-        <Nav className="me-auto">
-        <Nav.Link href="/home">User Profile</Nav.Link>
-        <Nav.Link href="/home">Settings</Nav.Link>
-        <Nav.Link href="/home">Weather Location</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>      
-  )
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/home">Weather With Me</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/home">User Profile</Nav.Link>
+            <Nav.Link href="/home">Settings</Nav.Link>
+            <Nav.Link href="/home">Weather Location</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      {props.children}
+    </>
+  );
 }
 
 // Usage in other pages: return <NavBar> <Component> </NavBar>
