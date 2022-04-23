8cb583c88c283da38d07060a3a8f50f24d187e7e
Emilia Tan <leeyat790@gmail.com>
2022-04-24 03:57:32 +0800

    Updated Login and SignUp UI

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import BACKEND_HOST from "../../frontendConfig";
+import checkString from "../../utils/checkString";
 import { Form, Card, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 const SignUp = () => {
@@ -15,9 +16,31 @@ const SignUp = () => {
     confirmedPassword: false,
     email: false,
   });
+
   const navigate = useNavigate();
   const createNewUser = async () => {
-    //TODO: validate the user info and add appropriate errors
+    //TODO: set all error to false before checking
+    //TODO; output error message
+    //Checked username format error, pw format error, username matched exisiting user error and confirmedPassword error
+    const { username, password, confirmedPassword } = userInfo;
+    const usernameCheckResult = checkString(username);
+    const passwordCheckResult = checkString(password);
+    if (!usernameCheckResult.success) {
+      setError({ ...error, username: usernameCheckResult.error });
+      return;
+    }
+    if (!passwordCheckResult.success) {
+      setError({ ...error, password: passwordCheckResult.error });
+      return;
+    }
+    if (confirmedPassword !== password) {
+      setError({
+        ...error,
+        confirmedPassword:
+          "The confirmed Password is different from your password.",
+      });
+      return;
+    }
     const url = `${BACKEND_HOST}/signup`;
     const payload = {
       method: "POST",
@@ -44,50 +67,81 @@ const SignUp = () => {
   };
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Body>
-        <Card.Title>Create Account</Card.Title>
-        <Form>
-          <Form.Control
-            type="text"
-            placeholder="Username"
-            onChange={(event) =>
-              setUserInfo({ ...userInfo, username: event.target.value })
-            }
-          />
-          <Form.Control
-            type="text"
-            placeholder="Password"
-            onChange={(event) =>
-              setUserInfo({ ...userInfo, password: event.target.value })
-            }
-          />
-          <Form.Control
-            type="text"
-            placeholder="Confirm Password"
-            onChange={(event) =>
-              setUserInfo({
-                ...userInfo,
-                confirmedPassword: event.target.value,
-              })
-            }
-          />
-          <Form.Control
-            type="text"
-            placeholder="Email "
-            onChange={(event) =>
-              setUserInfo({
-                ...userInfo,
-                confirmedPassword: event.target.value,
-              })
-            }
-          />
-        </Form>
-        <Button variant="primary" onClick={createNewUser}>
-          Login
-        </Button>
-      </Card.Body>
-    </Card>
+    <div className="d-flex justify-content-center" style={{ height: "100vh" }}>
+      <div className="d-flex align-items-center">
+        <Card style={{ width: "25rem", height: "40rem" }}>
+          <Card.Body>
+            <div
+              style={{ height: "10%", marginBottom: "5%" }}
+              className="d-flex justify-content-center align-items-center"
+            >
+              <Card.Title style={{ fontSize: "25px" }}>
+                Create Account
+              </Card.Title>
+            </div>
+            <Form style={{ height: "60%" }}>
+              <Form.Control
+                style={{ width: "100%", height: "15%", marginBottom: "5%" }}
+                type="text"
+                className={error.username ? "is-invalid" : ""}
+                placeholder="Username"
+                onChange={(event) =>
+                  setUserInfo({ ...userInfo, username: event.target.value })
+                }
+              />
+              <Form.Control
+                type="password"
+                style={{ width: "100%", height: "15%", marginBottom: "5%" }}
+                placeholder="Password"
+                onChange={(event) =>
+                  setUserInfo({ ...userInfo, password: event.target.value })
+                }
+              />
+              <Form.Control
+                type="password"
+                style={{ width: "100%", height: "15%", marginBottom: "5%" }}
+                placeholder="Confirm Password"
+                className={error.confirmedPassword ? "is-invalid" : ""}
+                onChange={(event) =>
+                  setUserInfo({
+                    ...userInfo,
+                    confirmedPassword: event.target.value,
+                  })
+                }
+              />
+              <Form.Control
+                type="text"
+                style={{ width: "100%", height: "15%" }}
+                placeholder="Email (Optional) "
+                onChange={(event) =>
+                  setUserInfo({
+                    ...userInfo,
+                    confirmedPassword: event.target.value,
+                  })
+                }
+              />
+            </Form>
+            <div style={{ height: "20%" }}>
+              <Button
+                style={{ width: "100%", height: "40%" }}
+                variant="primary"
+                onClick={createNewUser}
+              >
+                Create
+              </Button>
+            </div>
+            <div
+              style={{ height: "7%" }}
+              className="d-flex justify-content-end"
+            >
+              <Button variant="light" onClick={() => navigate("/login")}>
+                Back
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
   );
 };
 
