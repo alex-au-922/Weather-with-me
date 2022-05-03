5ac56f0b0c4279a7e9c1037f21a28ea7b491f1ac
desmondhth <desmond.hth@gmail.com>
2022-05-03 14:27:54 +0800

    search function updated

--- frontend/src/pages/SearchLocation/index.js
+++ frontend/src/pages/SearchLocation/index.js
@@ -10,6 +10,27 @@ import "bootstrap/dist/css/bootstrap.css";
 import objectSetAll from "../../utils/setAll";
 
 const Search = () => {
+    window.onload = function(){
+        const searchButton = document.getElementById('search-button');
+        const searchInput = document.getElementById('search-input');
+        searchButton.addEventListener('click', () => {
+        const inputValue = searchInput.value;
+        alert(inputValue);
+        });
+    }
+    return (
+    <>
+    <div class="input-group">
+        <div class="form-outline">
+            <input id="search-input" type="search" class="form-control" />
+            <label class="form-label" for="form1">test</label>
+        </div>
+            <button id="search-button" type="button" class="btn btn-primary">
+            <i class="fas fa-search"></i>
+        </button>
+    </div>
+  </>
+    )
 }
 
 export default Search;
