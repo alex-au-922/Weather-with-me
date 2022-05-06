c4495c6bdbbc0e7b2770bb2e749065311bc86e73
ethanlee928 <ethan2000.el@gmail.com>
2022-05-07 00:54:21 +0800

    added google map api in frontend for testing

--- frontend/src/pages/Home/userHome.js
+++ frontend/src/pages/Home/userHome.js
@@ -4,6 +4,33 @@ import { WeatherWebSocketContext } from "../../middleware/websocket";
 import { registerMessageListener } from "../../utils/listeners/webSocketMessage";
 import parseWeatherDataFrontendView from "../../utils/data/weather";
 import { Typeahead } from "react-bootstrap-typeahead";
+import { GoogleMap, LoadScript, InfoBox, OverlayView } from '@react-google-maps/api';
+import GOOGLE_API_KEY from "../../keys/googleAPI";
+
+// MAP SIZE ON SCREEN
+const containerStyle = {
+  width: '700px',
+  height: '700px'
+};
+
+// MAP CENTER LOCATION
+const center = {
+  lat: 22.30,
+  lng: 114.177216
+};
+
+const options = { closeBoxURL: '', enableEventPropagation: true };
+
+const onClick = () => {
+  console.info('I have been clicked!')
+};
+
+const divStyle = {
+  background: 'white',
+  border: '1px solid #ccc',
+  padding: 15
+};
+
 const UserView = (props) => {
   const { username, logout } = props;
   const [weatherList, setWeatherList] = useState();
@@ -27,11 +54,24 @@ const UserView = (props) => {
     return registerMessageListener(webSocket, handler);
   }, [webSocket]);
   return (
-    <Container>
-      <Row>
-        <div> Hello {username}!</div>
-      </Row>
-    </Container>
+    <LoadScript googleMapsApiKey={GOOGLE_API_KEY}>
+      <Container>
+        <Row>
+          <div> Hello {username}!</div>
+        </Row>
+        <GoogleMap mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10} >
+          <InfoBox options={options} position={center}>
+            <div style={{ backgroundColor: 'yellow', opacity: 0.75, padding: 12 }}>
+              <div style={{ fontSize: 16, fontColor: `#08233B` }}>
+                Cloudy day!
+              </div>
+            </div>
+          </InfoBox>
+        </GoogleMap>
+      </Container>
+    </LoadScript>
   );
 };
 
