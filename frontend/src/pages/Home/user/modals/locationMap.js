5bb9bf70a37ed7b5c740519d5d8e14d7273dc66e
cwjchin <cyrilchin282@gmail.com>
2022-05-11 00:18:14 +0800

    google map

---
+++ frontend/src/pages/Home/user/modals/locationMap.js
@@ -0,0 +1,165 @@
+import React, { useEffect, useState, useRef, useContext } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+import { registerMarkerListener } from "../../../../utils/listeners/googleMapMarkerListener"
+import GOOGLE_API_KEY from "../../../../keys/googleAPI";
+
+const loader = new Loader({
+  apiKey: GOOGLE_API_KEY,
+  version: "weekly",
+});
+
+const WeatherContent = (weatherData) => {
+  return `<ul style = "list-style: none;">
+        <li>Location: ${weatherData.name}</li>
+        <li>Temperature: ${weatherData.temperature}</li>
+        <li>Relative Humidity: ${weatherData.relativeHumidity} </li>
+        <li>Updated Time: ${weatherData.time}</li>
+    </ul>`;
+};
+
+const Marker = (props) => {
+  const [marker, setMarker] = useState({
+    marker: null,
+    option: null,
+  });
+  const [infoWindow, setInfoWindow] = useState({
+    infoWindow: null,
+    content: null,
+  });
+
+  const handleMouseOver = () => {
+    infoWindow.infoWindow.setContent(infoWindow.content);
+    infoWindow.infoWindow.open(props.googleMap, marker.marker);
+  };
+
+  const handleMouseOut = () => {
+    infoWindow.infoWindow.close(props.googleMap, marker.marker);
+  };
+
+  const handleMouseClick = () => {
+    //TODO: change the handle mouse click to render a consistent data view
+    console.log(marker.option);
+  };
+
+  useEffect(() => {
+    if (props.google !== null && props.data) {
+      setMarker({
+        ...marker,
+        option: {
+          position: new props.google.maps.LatLng(
+            props.data.longitude,
+            props.data.latitude
+          ),
+          visible: props.visible,
+          optimized: false,
+        },
+      });
+    }
+  }, [props.google, props.data]);
+  useEffect(() => {
+    if (props.visible) {
+      if (props.googleMap !== null && marker.option) {
+        if (!marker.marker) {
+          setMarker({
+            ...marker,
+            marker: new props.google.maps.Marker(marker.option),
+          });
+          setInfoWindow({
+            infoWindow: new props.google.maps.InfoWindow(),
+            content: WeatherContent(props.data),
+          });
+        }
+      }
+    }
+  }, [props.googleMap, marker.option, props.visible]);
+
+  useEffect(() => {
+    if (props.visible) {
+      if (marker.marker) {
+        marker.marker.setMap(props.googleMap);
+        const unregisterMouseOver = registerMarkerListener(
+          props.google,
+          marker.marker,
+          "mouseover",
+          handleMouseOver
+        );
+        const unregisterMouseOut = registerMarkerListener(
+          props.google,
+          marker.marker,
+          "mouseout",
+          handleMouseOut
+        );
+
+        const unregisterMouseClick = registerMarkerListener(
+          props.google,
+          marker.marker,
+          "click",
+          handleMouseClick
+        );
+
+        return () => {
+          console.log("remove!");
+          unregisterMouseOver();
+          unregisterMouseOut();
+          unregisterMouseClick();
+        };
+      }
+    } else {
+      console.log("remove due to visibility!");
+      marker.marker?.setMap(null);
+    }
+  }, [marker.marker, props.visible]);
+  return null;
+};
+
+const Map = (props) => {
+  const ref = useRef(null);
+  const [googleMap, setGoogleMap] = useState(null);
+  useEffect(() => {
+    if (ref.current && googleMap === null && props.google !== null) {
+        const defaultMapOptions = {
+            center: {
+              lat: props.weatherData.latitude,
+              lng: props.weatherData.longitude,
+            },
+            zoom: 4,
+          };
+        console.log(defaultMapOptions);
+      setGoogleMap(new props.google.maps.Map(ref.current, defaultMapOptions));
+    }
+    return () => console.log("removed map!");
+  }, [ref, props.google, googleMap]);
+  return (
+    <div ref={ref} style={{ width: "100%", height: "100%" }}>
+      {/* {<Marker
+          google={props.google}
+          googleMap={googleMap}
+          key={props.weatherData.name}
+          data={props.weatherData}
+        />
+      } */}
+    </div>
+  );
+};
+
+const LocationMapView = (props) => {
+  const [google, setGoogle] = useState(null);
+  useEffect(() => {
+    // initial load google
+    loader.load().then((google) => {
+      setGoogle(google);
+    });
+  }, []);
+
+  return (
+    <>
+      <Map
+        google={google}
+        weatherData={props.weatherData}
+      />
+      ;
+    </>
+  );
+};
+
+export default LocationMapView;
--- frontend/src/pages/Home/user/modals/weatherUserModal.js
+++ frontend/src/pages/Home/user/modals/weatherUserModal.js
@@ -1,84 +1,51 @@
-import camelToCapitalize from "../../../../utils/input/camelToCapitalize";
-import { FormRowHeader } from "../../../../utils/gui/formInputs";
+
 import { useState, useEffect } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
-import { InputFormModalRow, SelectFormModalRow } from ".";
-import { DeleteModal, UnsavedModal } from "../../../../utils/gui/modals";
+import "./weatherUserModal.css"
+import LocationMapView from "./locationMap";
 
 const WeatherUserLocationViewModal = (props) => {
   const [unsaved, setUnsaved] = useState(
     Object.keys(props.data).reduce((obj, key) => ((obj[key] = false), obj), {})
   );
-  const [showUnsavedModal, setShowUnsavedModal] = useState(false);
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [value, setValue] = useState(props.data);
-
-  const resetUnsaved = () => {
-    const resetUnsaved = Object.keys(props.data).reduce(
-      (obj, key) => ((obj[key] = false), obj),
-      {}
-    );
-    setUnsaved(resetUnsaved);
-  };
-
-  const handleChangeUnsaved = (field, changed) => {
-    const newUnsaved = { ...unsaved };
-    newUnsaved[field] = changed;
-    setUnsaved(newUnsaved);
-  };
-
-  const handleChangeValue = (field, changedValue) => {
-    const newValue = { ...value };
-    newValue[field] = changedValue;
-    setValue(newValue);
-  };
-
-  const handleShowUnsavedModal = () => setShowUnsavedModal(true);
-  const handleCloseUnsavedModal = () => setShowUnsavedModal(false);
-  const handleShowDeleteModal = () => setShowDeleteModal(true);
-  const handleCloseDeleteModal = () => setShowDeleteModal(false);
-
-  const handleInnerCloseModal = () => {
-    resetUnsaved();
-    props.onHide();
-  };
 
   const handleProperCloseModal = () => {
     const formUnsaved = Object.keys(unsaved).reduce(
       (accum, key) => accum || unsaved[key],
       false
     );
-    if (formUnsaved) {
-      handleShowUnsavedModal();
-    } else {
-      handleInnerCloseModal();
-    }
+    props.onHide();
   };
+
   return (
-    <>
       <Modal
-        show={props.show}
-        onHide={handleProperCloseModal}
-        centered
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        backdrop="static"
-        style={{ opacity: props.show ? (showUnsavedModal ? 0.8 : 1) : 0 }}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            User Data
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h1>Message!</h1>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => {}}>Save</Button>
-          <Button onClick={handleProperCloseModal}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+      show={props.show}
+      onHide={handleProperCloseModal}
+      centered
+      dialogClassName="modal-90w"
+      aria-labelledby="contained-modal-title-vcenter"
+      backdrop="static"
+    >
+      <Modal.Header closeButton>
+      </Modal.Header>
+      <Modal.Body>
+        <>
+          <div className="row">
+            <div className="column" style={{ width: "70%"}}>
+            <LocationMapView
+              weatherData={props.data}
+            />
+            </div>
+            <div className="column overflow" style={{ width: "30%"}}>
+            </div>
+          </div>
+        </> 
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleProperCloseModal}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
