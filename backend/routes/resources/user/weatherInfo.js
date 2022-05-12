cffa16de303f89a4350deed13b7a2eec5385a882
Emilia Tan <leeyat790@gmail.com>
2022-05-13 02:57:33 +0800

    updated admin log

--- backend/routes/resources/user/weatherInfo.js
+++ backend/routes/resources/user/weatherInfo.js
@@ -3,24 +3,42 @@ const {
   getLatestData: getLatestWeatherData,
   getLatestGeoLocationData,
   geoLocationToWeather,
+  getLatestBackupData,
 } = require("../../../databaseUtils/weatherDatabase/getLatestData");
 const usernameCheck = require("../../middleware/resourceAuth/usernameCheck");
+const {
+  findLocationInfoByName,
+} = require("../../../generalUtils/location/locationName.js");
 
 const router = express.Router();
 
 router.use("/", usernameCheck);
-router.get("/", async (req, res, next) => {
+router.get("/:location", async (req, res, next) => {
+  const location = req.params;
+  const locId = findLocationInfoByName(location).locationId;
+  const response = res.locals.response;
   try {
-    const response = res.locals.response;
-    const weatherResults = await getLatestWeatherData();
-    const geolocationResults = await getLatestGeoLocationData();
-    const newLatestWeatherData = geoLocationToWeather(
-      geolocationResults,
-      weatherResults
-    );
-    response.success = true;
-    response.result = newLatestWeatherData;
-    res.send(JSON.stringify(response));
+    if (location === undefined) {
+      const weatherResults = await getLatestWeatherData();
+      const geolocationResults = await getLatestGeoLocationData();
+      const newLatestWeatherData = geoLocationToWeather(
+        geolocationResults,
+        weatherResults
+      );
+      response.success = true;
+      response.result = newLatestWeatherData;
+      res.send(JSON.stringify(response));
+    } else {
+      const backupWeatherResults = await getLatestBackupData(locId);
+      const geolocationResults = await getLatestGeoLocationData();
+      const newLatestBackupWeatherData = geoLocationToWeather(
+        geolocationResults,
+        backupWeatherResults
+      );
+      response.success = true;
+      response.result = newLatestBackupWeatherData;
+      res.send(JSON.stringify(response));
+    }
   } catch (error) {
     next(error);
   }
--- frontend/src/pages/Home/admin/modals/logAdminModal.js
+++ frontend/src/pages/Home/admin/modals/logAdminModal.js
@@ -231,35 +231,37 @@ const LogAdminDataFormModal = (props) => {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            {Object.keys(props.data).map((field) => {
-              return (
-                <>
-                  {props.modalConfig[field].type === "select" ? (
-                    <SelectFormModalRow
-                      key={`${field}`}
-                      field={field}
-                      options={props.modalConfig[field].selectOptions}
-                      chosenOption={props.data[field]}
-                      onChangeUnsaved={handleChangeUnsaved}
-                      onChangeValue={handleChangeValue}
-                    />
-                  ) : (
-                    <InputFormModalRow
-                      key={`${field}`}
-                      field={field}
-                      type={props.modalConfig[field].type}
-                      mutable={props.modalConfig[field].unmutable}
-                      placeholder={camelToCapitalize(field)}
-                      blank={props.modalConfig[field].blank}
-                      value={props.data[field]}
-                      onChangeUnsaved={handleChangeUnsaved}
-                      onChangeValue={handleChangeValue}
-                    />
-                  )}
-                  <div className="mb-2" />
-                </>
-              );
-            })}
+            {Object.keys(props.data)
+              .filter((key) => props.modalConfig[key].display)
+              .map((field) => {
+                return (
+                  <>
+                    {props.modalConfig[field].type === "select" ? (
+                      <SelectFormModalRow
+                        key={`${field}`}
+                        field={field}
+                        options={props.modalConfig[field].selectOptions}
+                        chosenOption={props.data[field]}
+                        onChangeUnsaved={handleChangeUnsaved}
+                        onChangeValue={handleChangeValue}
+                      />
+                    ) : (
+                      <InputFormModalRow
+                        key={`${field}`}
+                        field={field}
+                        type={props.modalConfig[field].type}
+                        mutable={props.modalConfig[field].unmutable}
+                        placeholder={camelToCapitalize(field)}
+                        blank={props.modalConfig[field].blank}
+                        value={props.data[field]}
+                        onChangeUnsaved={handleChangeUnsaved}
+                        onChangeValue={handleChangeValue}
+                      />
+                    )}
+                    <div className="mb-2" />
+                  </>
+                );
+              })}
           </Form>
         </Modal.Body>
         <Modal.Footer>
@@ -286,26 +288,31 @@ const logModalOptions = {
     mutable: false,
     blank: false,
     type: "text",
+    display: false,
   },
   method: {
     mutable: false,
     blank: false,
     type: "text",
+    display: true,
   },
   userAgent: {
     mutable: false,
     blank: false,
     type: "textarea",
+    display: true,
   },
   date: {
     mutable: false,
     blank: false,
     type: "text",
+    display: true,
   },
   ip: {
     mutable: false,
     blank: false,
     type: "text",
+    display: true,
   },
 };
 
