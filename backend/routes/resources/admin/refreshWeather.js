eecc24ffe4209dd194521a1ef968061791789504
cwjchin <cyrilchin282@gmail.com>
2022-05-12 17:53:40 +0800

    rename getLatestLogData to getLatestData in refreshWeather.js

--- backend/routes/resources/admin/refreshWeather.js
+++ backend/routes/resources/admin/refreshWeather.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const getLatestLogData =
-  require("../../../databaseUtils/logDatabase/getLatestLogData").getLatestLogData;
+const getLatestData =
+  require("../../../databaseUtils/logDatabase/getLatestData").getLatestData;
 
 router.get("/", async (req, res, next) => {
   try {
